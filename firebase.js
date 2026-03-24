0d73fd480a5329f2ec2c6818c7abd6190df80c14
dcastillop09 <129134458+dcastillop09@users.noreply.github.com>
2026-03-24 17:26:43 -0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,42 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGUtJbi11RIO4G68RkCBg88BtX5QbMBC8",
+  authDomain: "control-finanzas-f9590.firebaseapp.com",
+  projectId: "control-finanzas-f9590",
+  storageBucket: "control-finanzas-f9590.firebasestorage.app",
+  messagingSenderId: "587555621895",
+  appId: "1:587555621895:web:b616562cdf302b84f0eea2",
+  measurementId: "G-NJD0NEDRWK"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export {
+  auth,
+  db,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  collection,
+  addDoc,
+  getDocs
+};
+
+
